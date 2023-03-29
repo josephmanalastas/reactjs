e1a1ef9d0b398c9eabff6a926ac7a4c5f48d4855
josephmanalastas <josmanalastas2023@gmail.com>
2023-03-30 00:34:17 +1100

    Initial commit

---
+++ precache-manifest.8dc6d2871cf67417919115fd045570c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c543938b8949366a8f1a70cbbb1628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "238f2db6e009c2bf14ee",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f583bb9da03d5431dacb",
+    "url": "/static/js/2.085957d3.chunk.js"
+  },
+  {
+    "revision": "238f2db6e009c2bf14ee",
+    "url": "/static/js/main.daddfd2c.chunk.js"
+  },
+  {
+    "revision": "153f9a281206f8088016",
+    "url": "/static/js/runtime-main.215ee531.js"
+  }
+]);
